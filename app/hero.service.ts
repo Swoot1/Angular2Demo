dc5f5f48b5e15d2cc16859b1365e5b4a1da54b64
elinnilsson <elinhejnilsson@gmail.com>
2016-01-06 20:44:54 +0100

    Implemented the super hero example from the Angular 2 docs.

---
+++ app/hero.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from 'angular2/core';
+import {HEROES} from './mock-heroes';
+
+@Injectable()
+export class HeroService{
+    
+    getHeroes(){
+        return Promise.resolve(HEROES);
+    }
+    
+    getHeroesSlowly() {
+      return new Promise<Hero[]>(resolve =>
+        setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+      );
+    }
+}
---
+++ app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import {Hero} from './hero';
+
+export var HEROES: Hero[] = [
+        {
+            id: 1,
+            name: 'Ms. Nice'
+        },
+        {
+            id: 2,
+            name: 'Batman'
+        },
+        {
+            id: 3,
+            name: 'Superman'
+        },
+        {
+            id: 4,
+            name: 'CatWoman'
+        },
+        {
+            id: 5,
+            name: 'Ms. America'
+        }
+    ];
