10cd9ac7d956a4ec4f02e413b5d33a5183a4b63f
elinnilsson <elinhejnilsson@gmail.com>
2016-01-09 11:19:42 +0100

    Removed most of app to get the basics.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,58 +1,9 @@
 import {Component} from 'angular2/core';
-import {TaskService} from './task.service';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>My Todo App</h1>
-        <input type="text" [(ngModel)]="newTask.name" />
-        <input type="date" [(ngModel)]="newTask.dueDate" />
-        <button class="btn btn-success"(click)="addTask(newTask)">Add task</button>
-        <table class="table-striped">
-            <thead>
-                <tr>
-                    <th>Task</th>
-                    <th>Status</th>
-                    <th>Due date</th>
-                    <th></th>
-                </tr>
-            </thead>    
-            <tbody>
-                <tr *ngFor="#task of tasks">
-                    <td>{{task.name }} </td>
-        <td><span [class.done]="task.done">{{task.done ? 'DONE' : 'I\\'M WORKING ON IT'}} </span></td>
-                    <td>{{task.dueDate}}</td>
-                    <td><button class="btn btn-primary" (click)="task.done = !task.done">{{task.done ? 'Undo' : 'Done'}}</button></td>
-                </tr>
-            </tbody>
-        </table>
-    `,
-    styles:[`
-        .done {
-            color: green
-        }
-    `],
-    providers: [TaskService]
+    template: '<h1>My Todo App</h1>'
     })
-
 export class AppComponent {
     
-    public tasks = [];
-    public newTask = {done: false};
-
-    constructor(private _taskService: TaskService){}
-    
-    ngOnInit(){
-        this.getTasks();
-    }
-    
-    getTasks(){
-        this._taskService.getTasks().then(tasks => this.tasks = tasks;);
-    }   
-    
-    addTask(task){
-        if(task){
-            this._taskService.addTask(task);
-            this.newTask = {done: false};
-        }
-    }   
 }
--- app/task.ts
+++
@@ -1,5 +0,0 @@
-export interface Task{
-    name: string;
-    done: boolean;
-    dueDate: Date;
-}
